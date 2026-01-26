72f295b96828b09d37ae16edac29d4b56c3dee5a
unknown <praveenkpraveen1020@gmail.com>
2026-01-26 11:54:10 +0530

    Dockerization Done

--- backend-drf/clickmart_main/settings.py
+++ backend-drf/clickmart_main/settings.py
@@ -138,6 +138,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR/'static'
+STATICFILES_DIRS = [
+    'clickmart_main/static'
+]
 
 
 MEDIA_URL = "/media/"
